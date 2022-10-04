274a82c448d19f22412c8c33a0c69b88d6768cf9
Vladimir Karas <avg151@ya.ru>
2022-10-04 13:45:57 +0300

    Calculate tip and set to Text

--- app/src/main/java/com/example/composetiptimesample/TipTimeScreen.kt
+++ app/src/main/java/com/example/composetiptimesample/TipTimeScreen.kt
@@ -6,13 +6,16 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetiptimesample.ui.theme.ComposeTipTimeSampleTheme
+import java.text.NumberFormat
 
 @Composable
 fun TipTimeScreen() {
@@ -30,10 +33,13 @@ fun TipTimeScreen() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        var amount by remember { mutableStateOf("") }
+        var amountInput by remember { mutableStateOf("") }
+        val amountDouble = amountInput.toDoubleOrNull() ?: 0.0
+        val calculateTip = calculateTip(amountDouble)
+
         TextField(
-            value = amount,
-            onValueChange = { amount = it },
+            value = amountInput,
+            onValueChange = { amountInput = it },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
@@ -46,7 +52,22 @@ fun TipTimeScreen() {
             }
         )
 
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = stringResource(id = R.string.tip_amount, calculateTip),
+            modifier = Modifier.align(CenterHorizontally),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+
     }
+}
+
+private fun calculateTip(amount: Double, percentage: Double = 15.0): String {
+    val tip = percentage / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
